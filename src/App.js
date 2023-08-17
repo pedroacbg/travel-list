3288dbac892acd830e9fe7baaafe76f58e41545a
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2023-08-17 09:59:59 -0300

    adding items

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Chrager", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -22,12 +28,31 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>You have X items on your list and you already packed X (X%)¨💯</em>
     </footer>
   );
